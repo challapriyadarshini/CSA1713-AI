6874ed06cbb70f7d14a45995625a8fd43f9337f2
challapriyadarshini <162314028+challapriyadarshini@users.noreply.github.com>
2025-05-27 11:19:42 +0530

    Add files via upload

---
+++ planet.pl
@@ -0,0 +1,10 @@
+% planet(Name, Diameter_km, DistanceFromSun_million_km, NumberOfMoons).
+
+planet(mercury, 4879, 57.9, 0).
+planet(venus, 12104, 108.2, 0).
+planet(earth, 12756, 149.6, 1).
+planet(mars, 6792, 227.9, 2).
+planet(jupiter, 142984, 778.5, 79).
+planet(saturn, 120536, 1434.0, 83).
+planet(uranus, 51118, 2871.0, 27).
+planet(neptune, 49528, 4497.1, 14).
